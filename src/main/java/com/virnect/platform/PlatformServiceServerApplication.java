89f9acc389ea0a00c3953fe111618bbae844e644
Jintae-VIRNECT <93517034+Jintae-VIRNECT@users.noreply.github.com>
2024-01-23 17:25:46 +0900

    first commit

---
+++ src/main/java/com/virnect/platform/PlatformServiceServerApplication.java
@@ -0,0 +1,13 @@
+package com.virnect.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformServiceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformServiceServerApplication.class, args);
+	}
+
+}
